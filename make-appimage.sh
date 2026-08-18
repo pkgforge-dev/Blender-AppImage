ec332897225b0479b5f9f6a00fd12e8166df67c1
Link4Electronics <link4electronics@gmail.com>
2026-08-18 15:29:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,7 @@ VERSION=$(pacman -Q blender | awk '{print $2; exit}') # example command to get v
 VERSION=${VERSION#*:}
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/blender.svg
 export DESKTOP=/usr/share/applications/blender.desktop
